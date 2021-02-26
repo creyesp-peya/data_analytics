ca307b63b30c9407a382bd2d693d336b99a31909
creyesp-peya <cesar.reyes@pedidosya.com>
2021-02-26 12:13:40 -0300

    remove no pkg files

--- inicialization_scripts/peya-ai-notebook-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-conda init
-conda activate base
-conda install papermill=2.2.2 -y
-conda install pandas-gbq -y 
-conda install python-crontab=2.5.1 -y
-conda install nb_conda_kernels=2.3.1 -y
-
-echo '{"CondaKernelSpecManager": {"kernelspec_path": "--user"}}' > $(jupyter --config-dir)/jupyter_config.json
